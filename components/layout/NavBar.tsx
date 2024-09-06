5e6889d416d4746a8f5819702bb6d094adde7465
horidianx69 <ayush.shuklaps@gmail.com>
2024-09-06 23:12:55 +0530

    NavBar search and signIn/out button

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,15 +1,49 @@
 "use client"
 
-import {UserButton} from "@clerk/nextjs";
-
+import { useAuth, UserButton } from "@clerk/nextjs";
+import Container from "../Container";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import SearchInput from "../SearchInput";
 
 const NavBar = () => {
+  const router = useRouter(); // Call useRouter to initialize the router object
+  const {userId} = useAuth(); // Call useAuth to initialize the user object
+
+
   return (
-     <header className="flex justify-between">
-              <h1>BabylonBookings</h1>
-              <UserButton showName/>
-            </header>
-  )
-}
+     <div className="sticky top-0 border border-b-primary/10 bg-secondary/50">
+       <Container>
+        <div className="flex items-center justify-between ">
+         <div 
+           className="flex items-center gap-1 cursor-pointer" 
+           onClick={() => router.push('/')}> {/* Use the router object */}
+           
+           <Image src="/logo.svg" alt="logo" width="35" height="35" />
+           <div className="font-semibold text-l p-2">BabylonBookings</div>
+         </div>
+
+          <div>
+            <SearchInput />
+          </div>
+          
+         <div className="flex gap-3 items-center">
+            <div>Theme</div>
+            <UserButton showName />
+            {!userId && <>
+              <Button onClick={()=> router.push('/sign-in')} variant='outline' size='sm'>Sign In</Button>
+              <Button onClick={()=> router.push('/sign-up')} size='sm'>Sign Up</Button>
+            </>}
+         </div>
+        </div>
+       </Container>
+     </div>
+  );
+};
 
 export default NavBar;
+
+
+// the navbar componenet includes the UserButton component from the clerk,which provides interactivity for the user to sign in or sign out of the application.
+//components that involve interactivity ,state changes need to rendered client side while in nextjs all componenets are rendered server side.
