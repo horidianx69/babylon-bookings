f74328a2c5f51fbece5fb82ea98ad734efca7dff
horidianx69 <ayush.shuklaps@gmail.com>
2024-09-27 22:28:09 +0530

    midsem❤️

--- app/api/hotel/route.ts
+++ app/api/hotel/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server"
 
 export async function Post(req: Request) {
-    // try{
-    //     const body = await req.json();
-    // }catch(error){
-    //     console.log ("Error at /api/hotel POST",error)
-    //     return new NextResponse('Internal Server Error',{status:500})
-    // }
+    try{
+        const body = await req.json();
+    }catch(error){
+        console.log ("Error at /api/hotel POST",error)
+        return new NextResponse('Internal Server Error',{status:500})
+    }
 }
